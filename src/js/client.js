cd991d3b29132029f915bdabb18764e4f3cb9cd5
Christopher Van <cvan@mozilla.com>
2014-09-11 17:03:34 -0700

    add PeerJS boilerplate code to use WebRTC data channels (issue #10)

---
+++ src/js/client.js
@@ -0,0 +1,38 @@
+// var peer = require('./lib/peer');
+// var Promise = require('./lib/promise-1.0.0');  // jshint ignore:line
+var settings = require('./settings');
+var utils = require('./lib/utils');
+var error = utils.error;
+var trace = utils.trace;
+
+
+// if there's not a pin, tell the user to open the game on another device
+// first. instead of relegating mobile to be always a controller, allow the
+// game to mirror the desktop (à la WiiU).
+
+var pins = utils.getPins();
+
+var peer = new Peer(pins.controller, {
+  key: settings.PEER_KEY,
+  debug: settings.DEBUG ? 3 : 0
+});
+
+var conn = peer.connect(pins.host);
+
+
+conn.on('open', function () {
+  trace('My peer ID: ' + peer.id);
+  trace('My connection ID: ' + conn.id);
+
+  conn.on('data', function (data) {
+    trace('Received: ' + data);
+  });
+
+  conn.on('error', function (err) {
+    error(err.message);
+  });
+
+  conn.send({
+    status: 'ready'
+  });
+});
---
+++ src/js/lib/utils.js
@@ -0,0 +1,45 @@
+function trace(text, level) {
+  console[level || 'log']((window.performance.now() / 1000).toFixed(3) + ': ' + text);
+}
+
+
+function error(text) {
+  return trace(text, 'error');
+}
+
+
+function warn(text) {
+  return trace(text, 'warn');
+}
+
+
+function getPins() {
+  var pin = (window.location.pathname.indexOf('.html') ?
+    window.location.search.substr(1) : window.location.pathname.substr(1));
+
+  var pins = {
+  	host: pin,
+  	controller: pin
+  };
+
+  if (!pin) {
+  	return pins;
+  }
+
+  // Prepend `host_` to host's ID.
+  if (pin.substr(0, 11) !== 'host_') {
+    pins.host = 'host_' + pins.host;
+  }
+
+  // Prepend `controller_` to controller's ID.
+  if (pin.substr(0, 11) !== 'controller_') {
+    pins.controller = 'controller_' + pins.controller; 
+  }
+
+  return pins;
+}
+
+module.exports.trace = trace;
+module.exports.error = error;
+module.exports.warn = warn;
+module.exports.getPins = getPins;
